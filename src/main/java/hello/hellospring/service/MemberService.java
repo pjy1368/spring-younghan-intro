09d2c8578808521aa9257d1fc430bc1283ed9b16
JinYoung Park <basejin0810@naver.com>
2021-05-03 16:32:06 +0900

    feat: 자동 의존 관계를 통해 스프링 빈에 등록

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,17 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(final MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
